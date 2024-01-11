c7ebda7bdba651d6d497e78ace3186fa840baf1e
Pedro741-code <loschiavopedro@gmail.com>
2024-01-11 19:53:33 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ switch (restaurnteEscolhidos) {
         break 
 
 
-        case "brasileira":
+        case "Brasileiro":
             console.log("pedir feijoda");
             break
 
@@ -17,23 +17,28 @@ switch (restaurnteEscolhidos) {
                 break
 }
 let sobremesaRestaurantes = "Pudim"
-if (restaurnteEscolhidos === "brasileiro"){
+if (restaurnteEscolhidos === "Brasileiro"){
     console.log("Aqui temos sua sobremesa: "+ sobremesaRestaurantes)
 }else{
     console.log("Nao temos essa sobremesa no menu")
 }
-for(let i=0; i<7; i++){
+let i = 6 
   if  (i%2===0){
     console.log("voces vao sentar em uma mesa redonda, pois voces sao mais de 5")
 
   }else{
     console.log("Nao temos mesas pequenas, pode ser o balcao?")
   }
-}
-let pessoasNaMesa = 7
-while (pessoasNaMesa%2===1){
+
+let pessoasNaMesa = 6
+while (pessoasNaMesa%2===1 && pessoasNaMesa<6){
     console.log("Está faltando alguém ")
-    break
+    if(pessoasNaMesa>6){
+        console.log("quem é esse na nossa mesa?")
+
+    }else if (pessoasNaMesa===6){
+        console.log("esta todos aqui")
+    }
 
 }
 
